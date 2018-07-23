d820ec0353a9410155efc4b6221a0099d3e116d0
Richard Fenix <richard.fenix@gmail.com>
2018-07-23 01:57:31 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
+require 'pry'
+
 class Pokemon
+  
+  attr_accessor :name, :type
+  
+  @@all = []
+ 
+  def initialize(name, type, id=nil)
+    @id = id
+    @name = name
+    @type = type
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def self.save(name, type)
+    @db.execute("INSERT INTO pokemon (name, type) VALUES (?,?", self.name, self.type)
+  end
+  
 end
